006e2faf35e79b47c144a47b0f579634d1048c94
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-09-18 20:04:50 +0500

    feat: added about-project component

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,23 @@
+import './AboutProject.css';
+
+function AboutProject() {
+  return (
+    <div className='aboutproject'>
+      <h2 className='aboutproject__heading'>О проекте</h2>
+      <div className='aboutproject__info'>
+        <p className='aboutproject__title'>Дипломный проект включал 5 этапов</p>
+        <p className='aboutproject__title'>На выполнение диплома ушло 5 недель</p>
+        <p className='aboutproject__content'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+        <p className='aboutproject__content'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>      
+      </div>
+      <div className='aboutproject__duration'>
+        <div className='aboutproject__stage-duration aboutproject__one-week-duration'>1 неделя</div>
+        <div className='aboutproject__stage-duration aboutproject__four-week-duration'>4 недели</div>
+        <div className='aboutproject__stage-name'>Back-end</div>
+        <div className='aboutproject__stage-name'>Front-end</div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutProject;
