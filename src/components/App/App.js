7f44c95142a102b6cf191859f1ac400629e08c68
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-09-18 23:07:55 +0500

    feat: added app component

---
+++ src/components/App/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Header from '../Header/Header';
+import Promo from '../Promo/Promo';
+import NavTab from '../NavTab/NavTab';
+import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
+import AboutMe from '../AboutMe/AboutMe';
+import Portfolio from '../Portfolio/Portfolio';
+import Footer from '../Footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Promo />
+      <NavTab />
+      <AboutProject />
+      <Techs />
+      <AboutMe />
+      <Portfolio />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
