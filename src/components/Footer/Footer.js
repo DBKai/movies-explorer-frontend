49a5b8a66fb864d6bd3af1041f4149e3adddeb40
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-10-01 23:15:15 +0500

    feat: changed footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,16 +2,28 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <div className='footer'>
+    <section className='footer'>
       <p className='footer__project'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
       <div className='footer__container'>
       <p className='footer__copyright'>© 2022</p>
         <div className='footer__links'>
-          <p className='footer__orgname'>Яндекс.Практикум</p>
-          <p className='footer__github'>Github</p>
+          <a 
+            className='footer__link' 
+            href='https://practicum.yandex.ru'
+            target='_blank' 
+            rel='noopener noreferrer'>
+            Яндекс.Практикум
+          </a>
+          <a 
+            className='footer__link' 
+            href='https://github.com/dbkai'
+            target='_blank' 
+            rel='noopener noreferrer'>
+            Github
+          </a>
         </div>
       </div>      
-    </div>
+    </section>
   );
 }
 
