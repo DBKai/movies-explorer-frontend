487e5407bf871c999f3dd1cae9c32f33d734ce8b
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-10-01 23:19:15 +0500

    feat: added styles and markdown filtercheckbox

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -2,16 +2,20 @@ import './FilterCheckbox.css';
 import { useState } from 'react';
 
 function FilterCheckbox() {
-  const [checked, setChecked] = useState(false);
-
-  function checkedChanged() {
-    setChecked(!checked);    
-  }
+  const [isChecked, setIsChecked] = useState(false);
 
   return (
-    <div className={`filtercheckbox ${checked && 'filtercheckbox__disable'}`} onClick={checkedChanged}>
-      <div className={`tumbler ${checked && 'tumbler__disable'}`}  />
-    </div>
+    <label className="filtercheckbox">
+      <input
+        onChange={() => {
+          setIsChecked(!isChecked);
+        }}
+        type="checkbox"
+        className="invisible-filtercheckbox"
+      />
+      Короткометражки
+      <span className="visible-filtercheckbox" />    
+    </label>
   );
 }
 
