3873c9452b7a814c5ef5c43425df64503e8656af
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-10-01 23:12:43 +0500

    feat: changed aboutproject component

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -2,13 +2,17 @@ import './AboutProject.css';
 
 function AboutProject() {
   return (
-    <div className='aboutproject'>
+    <section className='aboutproject'>
       <h2 className='aboutproject__heading'>О проекте</h2>
       <div className='aboutproject__info'>
-        <p className='aboutproject__title'>Дипломный проект включал 5 этапов</p>
-        <p className='aboutproject__title'>На выполнение диплома ушло 5 недель</p>
-        <p className='aboutproject__content'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-        <p className='aboutproject__content'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>      
+        <div className='aboutproject__info-container'>
+          <p className='aboutproject__title'>Дипломный проект включал 5 этапов</p>
+          <p className='aboutproject__content'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>        
+        </div>
+        <div className='aboutproject__info-container'>
+          <p className='aboutproject__title'>На выполнение диплома ушло 5 недель</p>        
+          <p className='aboutproject__content'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        </div>        
       </div>
       <div className='aboutproject__duration'>
         <div className='aboutproject__stage-duration aboutproject__one-week-duration'>1 неделя</div>
@@ -16,7 +20,7 @@ function AboutProject() {
         <div className='aboutproject__stage-name'>Back-end</div>
         <div className='aboutproject__stage-name'>Front-end</div>
       </div>
-    </div>
+    </section>
   );
 }
 
