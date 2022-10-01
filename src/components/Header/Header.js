22e15ab1f3052ef245b12275443da1386875b586
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-10-01 23:06:06 +0500

    feat: changed header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,42 @@
 import './Header.css';
 
 import logo from '../../images/logo.svg';
+import { Link, useLocation } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation';
-import { useLocation } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import CurrentUserContext from '../../contexts/CurrentUserContext';
 
-function App() {
+function Header() {
   const location = useLocation();
-  const landingPage = location.pathname === '/';
-  const headerClassName = `header ${ landingPage && "header_dark" }`
-
+  const mainPage = location.pathname === '/';
+  const headerClassName = `header${ mainPage ? ' header_dark' : '' }`
+  const burgerMenuClassName = `header__burger-menu${ mainPage ? ' header__burger-menu-white' : '' }`;
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+  const isLoggedIn = useContext(CurrentUserContext);
+  
   return (
-    <div className={ headerClassName }>      
-      <img src={logo} alt='Логотип movies explorer' className='header__logo' />
-      <Navigation />
-    </div>
+    <section className={ headerClassName }>
+      <Link to='/' className='header__link-logo'>
+        <img src={logo} alt='Логотип movies explorer' className='header__logo' />
+      </Link>
+      {
+        !isLoggedIn ? 
+          <div className='header__login'>
+            <Link to='/signup' className='header__link header__link-signup'>Регистрация</Link>
+            <Link to='/signin' className='header__link header__link-signin'>Войти</Link>
+          </div> :
+          <>
+            <div 
+              className={burgerMenuClassName}
+              onClick={() => setIsMenuOpened(true)}></div>
+            <Navigation 
+              isOpen={isMenuOpened} 
+              onClose={() => setIsMenuOpened(false)} 
+              mainPage={mainPage} />
+          </>          
+      }
+    </section>
   );
 }
 
-export default App;
+export default Header;
