8c46f7f2a72303b1c6cb718a8a06052ae6ef6cf0
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-10-01 23:18:28 +0500

    feat: changed navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,27 +1,34 @@
 import './Navigation.css';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
-function Navigation() {
-  const isActive = ({ isActive }) => isActive && 'header__navigation-item_active';
-
-  return (
-    <div className='header__navigation-container'>
-        <nav className='header__navigation'>
-          <NavLink 
-            to='movies' 
-            className={`header__navigation-item ${isActive}`}>Фильмы</NavLink>
-          <NavLink 
-            to='saved-movies' 
-            className={`header__navigation-item ${isActive}`}>Сохранённые фильмы</NavLink>      
-        </nav>
-        {/* {          
-          <div className='header__login'>
-            <button className='header__link signup'>Регистрация</button>
-            <button to='/signin' className='header__link signin'>Войти</button>
-          </div>             
-        } */}
-        <button className='header__account'>Аккаунт</button>
+function Navigation({isOpen, onClose, mainPage}) {
+    const activeNavLinkClassName = ({isActive}) => {
+      return `navigation__item${mainPage ? ' navigation__item-white' : ''}${isActive ? ' navigation__item_active' : ''}`;
+    };
+    return (
+    <>
+      <div className={`navigation header__navigation${isOpen ? ' navigation_opened' : ''}`}>
+        <div className='navigation__container'>
+          <div className='navigation__link-container'>
+            <button className='navigation__menu-close' onClick={onClose}/>
+            <nav className='navigation__links'>
+              <NavLink 
+                to='/' 
+                className='navigation__item navigation__mobile-item'>Главная</NavLink>
+              <NavLink 
+                to='/movies'
+                className={activeNavLinkClassName}>Фильмы</NavLink>
+              <NavLink 
+                to='/saved-movies' 
+                className={activeNavLinkClassName}>Сохранённые фильмы</NavLink>      
+            </nav>
+          </div>
+          <Link 
+            to='/profile' 
+            className={`navigation__account${mainPage ? ' navigation__account-white' : ''}`}>Аккаунт</Link>
+        </div>
       </div>
+    </>
   );
 }
 
