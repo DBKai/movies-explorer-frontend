0b0306cd3786ec4c30e6cb2073ff414a97456eb0
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-10-01 23:14:05 +0500

    feat: changed aboutme component

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,20 +3,26 @@ import student from '../../images/student.jpg';
 
 function AboutMe() {
   return (
-    <div className='aboutme'>
+    <section className='aboutme'>
       <h2 className='aboutme__heading'>Студент</h2>
       <div className='aboutme__container'>
         <div className='aboutme__text'>
-          <div>
+          <div className='aboutme__text-container'>
             <p className='aboutme__name'>Дмитрий</p>
             <p className='aboutme__info'>Фронтенд-разработчик, 32 года</p>
             <p className='aboutme__description'>Я родился и живу в городе Сухой Лог, разрабатываю программы на стеке C# .NET Windows Forms. С 2021 решил поменять десктопные приложения на веб, начал учиться в Яндекс Практикуме. На фронтенде останавливаться не планирую, моя цель - fullstack developer.</p>
-          </div>          
-          <p className='aboutme__github'>github.com/DBKai</p>      
+          </div>
+          <a 
+            className='aboutme__link' 
+            href='https://github.com/dbkai'
+            target='_blank' 
+            rel='noopener noreferrer'>
+            Github
+          </a>    
         </div>
         <img className='aboutme__photo' src={student} alt='Фото студента'></img>
       </div>
-    </div>
+    </section>
   );
 }
 
