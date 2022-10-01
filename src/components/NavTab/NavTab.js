812099e308f55dea769b1e48b9c0397e3fe79a1b
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-10-01 23:11:34 +0500

    feat: added navtab component

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -3,12 +3,35 @@ import { Link } from 'react-scroll';
 
 function NavTab() {
 
-  return (    
-    <ul className='navtab'>
-      <li><Link to='aboutproject' className='navtab__link' spy={true} smooth={true} duration={500}>О проекте</Link></li>
-      <li><Link to='techs' className='navtab__link' spy={true} smooth={true} duration={500}>Технологии</Link></li>
-      <li><Link to='aboutme' className='navtab__link' spy={true} smooth={true} duration={500}>Студент</Link></li>
-    </ul>    
+  return (
+    <section className='navtab'>
+      <ul className='navtab__links'>
+        <li>
+          <Link 
+            to='aboutproject' 
+            className='navtab__link' 
+            spy={true} 
+            smooth={true} 
+            duration={500}>О проекте</Link>
+        </li>
+        <li>
+          <Link 
+            to='techs' 
+            className='navtab__link' 
+            spy={true} 
+            smooth={true} 
+            duration={500}>Технологии</Link>
+        </li>
+        <li>
+          <Link 
+            to='aboutme' 
+            className='navtab__link' 
+            spy={true} 
+            smooth={true} 
+            duration={500}>Студент</Link>
+          </li>
+      </ul>    
+    </section>    
   );
 }
 
