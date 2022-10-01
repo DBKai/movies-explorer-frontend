a46a01f0dfdd25cb973326dde0a36f17d27f4ed3
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-10-01 23:14:37 +0500

    feat: changed portfolio component

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,23 +2,41 @@ import './Portfolio.css';
 
 function Portfolio() {
   return (
-    <div className='portfolio'>
+    <section className='portfolio'>
       <h3 className='portfolio__heading'>Портфолио</h3>
       <ul className='portfolio__list'>
         <ol className='portfolio__item'>
-          <p className='portfolio__item-text'>Статичный сайт</p>
+          <a 
+            className='portfolio__item-link'
+            href='https://dkay.ru/how-to-learn' 
+            target='_blank' 
+            rel='noopener noreferrer'>
+            <p className='portfolio__item-text'>Статичный сайт</p>
+          </a>
+          <p className='portfolio__item-arrow'>↗</p>
+        </ol>
+        <ol className='portfolio__item'>
+          <a 
+            className='portfolio__item-link'
+            href='https://dkay.ru/russian-travel' 
+            target='_blank' 
+            rel='noopener noreferrer'>
+            <p className='portfolio__item-text'>Адаптивный сайт</p>
+          </a>
           <p className='portfolio__item-arrow'>↗</p>  
         </ol>
         <ol className='portfolio__item'>
-          <p className='portfolio__item-text'>Адаптивный сайт</p>
-          <p className='portfolio__item-arrow'>↗</p>  
-        </ol>
-        <ol className='portfolio__item'>
-          <p className='portfolio__item-text'>Одностраничное приложение</p>
+          <a 
+            className='portfolio__item-link'
+            href='https://dkay.ru/react-mesto-auth' 
+            target='_blank' 
+            rel='noopener noreferrer'>
+            <p className='portfolio__item-text'>Одностраничное приложение</p>
+          </a>
           <p className='portfolio__item-arrow'>↗</p>  
         </ol>
       </ul>
-    </div>
+    </section>
   );
 }
 
